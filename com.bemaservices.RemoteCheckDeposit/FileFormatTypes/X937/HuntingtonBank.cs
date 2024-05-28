67bed758154f0a46fb2db41022bbecf15ffa690f
David Turner <david@pillarsforrock.com>
2024-05-28 16:36:44 -0700

    Add Huntington Bank

---
+++ com.bemaservices.RemoteCheckDeposit/FileFormatTypes/X937/HuntingtonBank.cs
@@ -0,0 +1,263 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+using com.bemaservices.RemoteCheckDeposit.Records.X937;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Model;
+
+namespace com.bemaservices.RemoteCheckDeposit.FileFormatTypes
+{
+    /// <summary>
+    /// Defines the basic functionality of any component that will be exporting using the X9.37
+    /// DSTU standard.
+    /// </summary>
+    [Description( "Processes a batch export for Huntington Bank." )]
+    [Export( typeof( FileFormatTypeComponent ) )]
+    [ExportMetadata( "ComponentName", "Huntington Bank" )]
+
+    [EncryptedTextField( "Bank of First Deposit (BOFD) Routing Number", "", true, key: "BOFDRoutingNumber" )]
+
+    public class HuntingtonBank : X937DSTU
+    {
+        #region System Setting Keys
+
+        /// <summary>
+        /// The system setting for the next cash header identifier. These should never be
+        /// repeated. Ever.
+        /// </summary>
+        protected const string SystemSettingNextCashHeaderId = "HuntingtonBank.NextCashHeaderId";
+
+        /// <summary>
+        /// The system setting that contains the last file modifier we used.
+        /// </summary>
+        protected const string SystemSettingLastFileModifier = "HuntingtonBank.LastFileModifier";
+
+        /// <summary>
+        /// The last item sequence number used for items.
+        /// </summary>
+        protected const string LastItemSequenceNumberKey = "HuntingtonBank.LastItemSequenceNumber";
+
+        #endregion
+
+        /// <summary>
+        /// Gets the next item sequence number.
+        /// </summary>
+        /// <returns>An integer that identifies the unique item sequence number that can be used.</returns>
+        protected int GetNextItemSequenceNumber()
+        {
+            int lastSequence = GetSystemSetting( LastItemSequenceNumberKey ).AsIntegerOrNull() ?? 0;
+            int nextSequence = lastSequence + 1;
+
+            SetSystemSetting( LastItemSequenceNumberKey, nextSequence.ToString() );
+
+            return nextSequence;
+        }
+
+        /// <summary>
+        /// Gets the file header record (type 01).
+        /// </summary>
+        /// <param name="options">Export options to be used by the component.</param>
+        /// <returns>
+        /// A FileHeader record.
+        /// </returns>
+        protected override FileHeader GetFileHeaderRecord( ExportOptions options )
+        {
+            var header = base.GetFileHeaderRecord( options );
+
+            //
+            // The combination of the following fields must be unique:
+            // DestinationRoutingNumber + OriginatingRoutingNumber + CreationDateTime + FileIdModifier
+            //
+            // If the last file we sent has the same routing numbers and creation date time then
+            // increment the file id modifier.
+            //
+            var fileIdModifier = "A";
+            header.CountryCode = string.Empty;
+            var hashText = header.ImmediateDestinationRoutingNumber + header.ImmediateOriginRoutingNumber + header.FileCreationDateTime.ToString( "yyyyMMdd" );
+            var hash = HashString( hashText );
+
+            //
+            // find the last modifier, if there was one.
+            //
+            var lastModifier = GetSystemSetting( SystemSettingLastFileModifier );
+            if ( !string.IsNullOrWhiteSpace( lastModifier ) )
+            {
+                var components = lastModifier.Split( '|' );
+
+                if ( components.Length == 2 )
+                {
+                    //
+                    // If the modifier is for the same file, increment the file modifier.
+                    //
+                    if ( components[0] == hash )
+                    {
+                        fileIdModifier = ( ( char ) ( components[1][0] + 1 ) ).ToString();
+
+                        //
+                        // If we have done more than 26 files today, assume we are testing and start back at 'A'.
+                        //
+                        if ( fileIdModifier[0] > 'Z' )
+                        {
+                            fileIdModifier = "A";
+                        }
+                    }
+                }
+            }
+
+            header.FileIdModifier = fileIdModifier;
+            SetSystemSetting( SystemSettingLastFileModifier, string.Join( "|", hash, fileIdModifier ) );
+
+            header.CountryCode = string.Empty;
+
+            return header;
+        }
+
+        protected override CashLetterControl GetCashLetterControlRecord( ExportOptions options, List<Record> records )
+        {
+            var cashLeterControl = base.GetCashLetterControlRecord( options, records );
+            cashLeterControl.SettlementDate = null;
+            return cashLeterControl;
+        }
+
+        /// <summary>
+        /// Gets the cash letter header record (type 10).
+        /// </summary>
+        /// <param name="options">Export options to be used by the component.</param>
+        /// <returns>
+        /// A CashLetterHeader record.
+        /// </returns>
+        protected override CashLetterHeader GetCashLetterHeaderRecord( ExportOptions options )
+        {
+            int cashHeaderId = GetSystemSetting( SystemSettingNextCashHeaderId ).AsIntegerOrNull() ?? 1;
+
+            var header = base.GetCashLetterHeaderRecord( options );
+            header.ID = cashHeaderId.ToString( "D8" );
+            SetSystemSetting( SystemSettingNextCashHeaderId, ( cashHeaderId + 1 ).ToString() );
+
+            return header;
+        }
+
+        /// <summary>
+        /// Gets the bundle header record (type 20).
+        /// </summary>
+        /// <param name="options">Export options to be used by the component.</param>
+        /// <param name="bundleIndex">Number of existing bundle records in the cash letter.</param>
+        /// <returns>A BundleHeader record.</returns>
+        protected override Records.X937.BundleHeader GetBundleHeader( ExportOptions options, int bundleIndex )
+        {
+            var header = base.GetBundleHeader( options, bundleIndex );
+            header.ReturnLocationRoutingNumber = string.Empty;
+            return header;
+        }
+
+        /// <summary>
+        /// Gets the credit detail deposit record (type 61).
+        /// </summary>
+        /// <param name="options">Export options to be used by the component.</param>
+        /// <param name="bundleIndex">Number of existing bundle records in the cash letter.</param>
+        /// <param name="transactions">The transactions associated with this deposit.</param>
+        /// <returns>
+        /// A collection of records.
+        /// </returns>
+        protected override List<Record> GetCreditDetailRecords( ExportOptions options, int bundleIndex, List<FinancialTransaction> transactions )
+        {
+            //No Type 61 Record Needed
+            var records = new List<Record>();
+            
+            return records;
+        }
+
+        /// <summary>
+        /// Gets the records that identify a single check being deposited.
+        /// </summary>
+        /// <param name="options">Export options to be used by the component.</param>
+        /// <param name="transaction">The transaction to be deposited.</param>
+        /// <returns>
+        /// A collection of records.
+        /// </returns>
+        protected override List<Record> GetItemRecords( ExportOptions options, FinancialTransaction transaction )
+        {
+            var records = base.GetItemRecords( options, transaction );
+            var sequenceNumber = GetNextItemSequenceNumber();
+
+            //
+            // Modify the Check Detail Record and Check Image Data records to have
+            // a unique item sequence number.
+            //
+            var checkDetail = records.Where( r => r.RecordType == 25 ).Cast<Records.X937.CheckDetail>().FirstOrDefault();
+            checkDetail.ClientInstitutionItemSequenceNumber = sequenceNumber.ToString( "000000000000000" );
+            checkDetail.ElectronicReturnAcceptanceIndicator = "0";
+            checkDetail.MICRValidIndicator = 1; //Added 9/27/19 due to feedback from 5/3
+            checkDetail.BankOfFirstDepositIndicator = "U";
+            checkDetail.ArchiveTypeIndicator = string.Empty;
+
+
+            //Modify Check Detail Adden A
+            var checkDetailA = records.Where( r => r.RecordType == 26 ).Cast<Records.X937.CheckDetailAddendumA>().FirstOrDefault();
+            checkDetailA.BankOfFirstDepositRoutingNumber = Rock.Security.Encryption.DecryptString( GetAttributeValue( options.FileFormat, "BOFDRoutingNumber" ) );
+            checkDetailA.TruncationIndicator = "Y";
+            checkDetailA.BankOfFirstDepositItemSequenceNumber = sequenceNumber.ToString("000000000000000");
+            checkDetailA.BankOfFirstDepositCorrectionIndicator = "";
+            checkDetailA.BankOfFirstDepositAccountNumber = Rock.Security.Encryption.DecryptString(GetAttributeValue(options.FileFormat, "AccountNumber"));
+            checkDetailA.BankOfFirstDepositCorrectionIndicator = string.Empty; ;
+            foreach ( var imageData in records.Where( r => r.RecordType == 52 ).Cast<dynamic>() )
+            {
+                imageData.ClientInstitutionItemSequenceNumber = sequenceNumber.ToString( "000000000000000" );
+            }
+
+            return records;
+        }
+
+        /// <summary>
+        /// Gets the image record for a specific transaction image (type 50 and 52).
+        /// </summary>
+        /// <param name="options">Export options to be used by the component.</param>
+        /// <param name="transaction">The transaction being deposited.</param>
+        /// <param name="image">The check image scanned by the scanning application.</param>
+        /// <param name="isFront">if set to <c>true</c> [is front].</param>
+        /// <returns>A collection of records.</returns>
+        protected override List<Record> GetImageRecords( ExportOptions options, FinancialTransaction transaction, FinancialTransactionImage image, bool isFront )
+        {
+
+            var originRoutingNumber = Rock.Security.Encryption.DecryptString( GetAttributeValue( options.FileFormat, "AccountNumber" ) );
+
+            var records = base.GetImageRecords( options, transaction, image, isFront );
+
+            var detail = records.Where( r => r.RecordType == 50 ).Cast<ImageViewDetail>().FirstOrDefault();
+            if ( detail != null )
+            {
+                detail.ImageCreatorRoutingNumber = originRoutingNumber;
+            }
+
+            var data = records.Where( r => r.RecordType == 52 ).Cast<ImageViewData>().FirstOrDefault();
+            if ( data != null )
+            {
+                data.InstitutionRoutingNumber = originRoutingNumber;
+            }
+
+            return records;
+        }
+
+
+        /// <summary>
+        /// Hashes the string with SHA256.
+        /// </summary>
+        /// <param name="contents">The contents to be hashed.</param>
+        /// <returns>A hex representation of the hash.</returns>
+        protected string HashString( string contents )
+        {
+            byte[] byteContents = Encoding.Unicode.GetBytes( contents );
+
+            var hash = new System.Security.Cryptography.SHA256CryptoServiceProvider().ComputeHash( byteContents );
+
+            return string.Join( "", hash.Select( b => b.ToString( "x2" ) ).ToArray() );
+        }
+    }
+}
+
+
